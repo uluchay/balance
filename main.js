04208ed364e3a61fef2229e49b36220f129ec2ef
uluchay <42967843+uluchay@users.noreply.github.com>
2018-11-20 15:18:48 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+	hideEverything();
+	switchToHomeOut();
+	
+	$('.home-name').click(function(){
+		switchToHomeIn();
+	});	
+	
+});
+
+function switchToHomeOut(){
+	hideEverything();
+	$('.home-out').show();
+}
+function switchToHomeIn(){
+	hideEverything();
+	$('.home-in').show();
+}
+function switchToCreateAcc(){
+	hideEverything();
+	$('.create-account').show();
+}
+function switchToInsertExpense(){
+	hideEverything();
+	$('.insert-expense').show();
+}
+function switchToAccountHistory(){
+	hideEverything();
+	$('.account-history').show();
+}
+function hideEverything(){
+	$('.home-out').hide();
+	$('.home-in').hide();
+	$('.create-account').hide();
+	$('.insert-expense').hide();
+	$('.account-history').hide();
+}
+
+
+
+
+
+
+
+
